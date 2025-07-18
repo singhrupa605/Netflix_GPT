7bba150d089be31a5bdb9af0e52c54cebaf0ae9a
Rupa Singh <singhrupa605@gmail.com>
2025-07-18 16:19:12 +0530

    created recat project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+     <h1 className='text-gray-700 font-bold'>React + Vite </h1>
+     <img src={svg} />
+    </>
+  )
+}
+
+export default App
